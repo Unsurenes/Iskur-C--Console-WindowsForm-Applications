c9e135357ceeb2b4e2579cff93ecd125cda2e7ec
Unsurenes <92476437+Unsurenes@users.noreply.github.com>
2022-01-26 13:18:20 +0300

    Add files via upload

---
+++ Cekilis/Cekilis/IsimListe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cekilis
+{
+    class IsimListe
+    {
+        public string Ad;
+        public string[] isim = new string[50];
+
+        public void Ekle(int a, string ad)
+        {
+
+            this.Ad = ad;
+            isim[a] = ad;
+
+        }
+    }
+}
---
+++ Cekilis/Cekilis/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cekilis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsimListe liste = new IsimListe();
+
+            int devam;
+            do
+            {
+
+                Console.WriteLine("Çekilişe dahil etmek istediğiniz kişi sayısını giriniz.");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < sayi; i++)
+                {
+                    Console.Write(i + 1 + ". kişinin ismini giriniz:");
+                    string isim = Console.ReadLine();
+                    liste.Ekle(i, isim);
+
+                }
+                Console.WriteLine("Çekilişi başlatmak için tıklayınız");
+                Console.ReadLine();
+                Console.Clear();
+
+                Random rand = new Random();
+                List<int> randomlist = new List<int>();
+                for (int i = 0; i < sayi; i++)
+                {
+                    int c;
+                    do
+                    {
+                        c = rand.Next(sayi);
+                    } while (randomlist.Contains(c));
+                    randomlist.Add(c);
+                    Console.WriteLine(i + 1 + ". Talihli " + liste.isim[c]);
+                    Console.ReadLine();
+                }
+                Console.WriteLine("Çekiliş bitmiştir.Tekrarlamak için 1 çıkmak için 2'yi tuşlayınız");
+                devam = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+
+
+            } while (devam == 1);
+        }
+    }
+}
